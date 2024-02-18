458948bcc7e965423f64dc7d9f47dddeee9a74ae
Kael <qcherry2192@gmail.com>
2024-02-18 15:48:53 +0700

    First commit

--- back-end/index.js
+++ back-end/index.js
@@ -1,6 +1,19 @@
-const ethers = require("ethers")
+const ethers = require("ethers");
+const dotenv = require("dotenv");
+dotenv.config();
 
-async function main(){
-    
+let provider, wallet;
+async function init() {
+  provider = new ethers.providers.JsonRpcProvider("http://127.0.0.1:8545");
+  wallet = new ethers.Wallet(
+    "0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80",
+    provider
+  );
 }
-main()
+
+async function main() {
+  init();
+  console.log(await wallet.getAddress());
+  console.log((await wallet.getBalance()).toString());
+}
+main();
