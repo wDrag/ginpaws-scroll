f09cc9d56efe2031d5792534a148170010e267db
Kael <qcherry2192@gmail.com>
2024-02-20 13:07:21 +0700

    Update SC

--- contracts/src/LPAggreator.sol
+++ contracts/src/LPAggreator.sol
@@ -1,23 +1,23 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.24;
+pragma solidity ^0.7.6;
 
-// import "@uniswap/v3-core/contracts/interfaces/IUniswapV3Pool.sol";
-// import "@uniswap/v3-core/contracts/libraries/TickMath.sol";
-// import "@openzeppelin/contracts/token/ERC721/IERC721Receiver.sol";
-// import "@uniswap/v3-periphery/contracts/interfaces/ISwapRouter.sol";
-// import "@uniswap/v3-periphery/contracts/interfaces/INonfungiblePositionManager.sol";
-// import "@uniswap/v3-periphery/contracts/libraries/TransferHelper.sol";
-// import "@uniswap/v3-periphery/contracts/base/LiquidityManagement.sol";
+import "@uniswap/v3-core/contracts/interfaces/IUniswapV3Pool.sol";
+import "@uniswap/v3-core/contracts/libraries/TickMath.sol";
+import "@openzeppelin/contracts/token/ERC721/IERC721Receiver.sol";
+import "@uniswap/v3-periphery/contracts/interfaces/ISwapRouter.sol";
+import "@uniswap/v3-periphery/contracts/interfaces/INonfungiblePositionManager.sol";
+import "@uniswap/v3-periphery/contracts/libraries/TransferHelper.sol";
+import "@uniswap/v3-periphery/contracts/base/LiquidityManagement.sol";
 
-import "../lib/uniswap-v3-core/contracts/interfaces/IUniswapV3Pool.sol";
-import "../lib/uniswap-v3-core/contracts/libraries/TickMath.sol";
-import "../lib/openzeppelin-contracts/contracts/token/ERC721/IERC721Receiver.sol";
-import "../lib/uniswap-v3-periphery/contracts/interfaces/ISwapRouter.sol";
-import "../lib/uniswap-v3-periphery/contracts/interfaces/INonfungiblePositionManager.sol";
-import "../lib/uniswap-v3-periphery/contracts/libraries/TransferHelper.sol";
-import "../lib/uniswap-v3-periphery/contracts/base/LiquidityManagement.sol";
+// import "../lib/uniswap-v3-core/contracts/interfaces/IUniswapV3Pool.sol";
+// import "../lib/uniswap-v3-core/contracts/libraries/TickMath.sol";
+// import "../lib/openzeppelin-contracts/contracts/token/ERC721/IERC721Receiver.sol";
+// import "../lib/uniswap-v3-periphery/contracts/interfaces/ISwapRouter.sol";
+// import "../lib/uniswap-v3-periphery/contracts/interfaces/INonfungiblePositionManager.sol";
+// import "../lib/uniswap-v3-periphery/contracts/libraries/TransferHelper.sol";
+// import "../lib/uniswap-v3-periphery/contracts/base/LiquidityManagement.sol";
 
-contract LPAggreator is ERC721Receiver {
+contract LPAggreator is IERC721Receiver {
     INonfungiblePositionManager public immutable nonfungiblePositionManager;
 
     struct Deposit {
